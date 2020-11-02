2b23a83e727768eebc6357d6aa88af7d6e314187
dyh <dyh@123.com>
2020-11-02 18:19:43 +0800

    替换文件

--- test.rb
+++ test.rb
@@ -3,18 +3,11 @@
 require_relative './awdb/db.rb'
 
 
-filename = 'D:\\IP_city_2020W12_single_WGS84.awdb'
+filename = '../IP_city_single_WGS84_awdb.awdb'
 reader = AW::DB.new(filename)
 loc = reader.get('166.111.4.100')
-loc.each do |key, value|
-    if key == "multiAreas"
-        for area in value do
-            area.each do |key_area, value_area|
-                print "\t", key_area, " -> ", value_area, "\n"
-            end
-        end 
-    else
-        print key, " --> ", value, "\n"
-    end
-end
+
+puts "#{loc['areacode']}"
+puts "#{loc['continent']}"
+puts loc
 reader.close
